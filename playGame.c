22a5f2f3f3646987b5fc119f3a57e888f16112f8
ArawigaH <s1260162.k.cuo.kikakukaihatu@gmail.com>
2019-07-11 10:19:31 +0900

    First revision

---
+++ playGame.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    
+  int d1,d2,i;
+    int result;
+    unsigned int t;
+    
+    t = time(NULL);
+    srand(t);
+    
+    printf("Rolling the dice...\n");
+    
+    d1 = rand() % 6 + 1;
+    for(i=0;i<rand()%6+1;i++){
+    }
+    printf("Die 1: %d\n",d1);
+    
+    d2 = rand() % 6 + 1;
+    
+    printf("Die 2: %d\n",d2);
+    
+    result = d1 + d2;
+    
+    printf("Total value: %d\n",result);
+    
+    return 0;
+}
+
